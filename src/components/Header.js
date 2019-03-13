7458650ffdf32c852f96e103fd4399abd3fbffe9
Gel Studio <devondaviau@yahoo.ca>
2019-03-13 16:19:42 -0400

    build out mock nav to test scrollspy and perhaps style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Scrollspy from 'react-scrollspy'
 
 const StyledHeader = styled.header`
   background: ${props => props.theme.backgroundColor};
@@ -8,11 +9,20 @@ const StyledHeader = styled.header`
 const Header = () => {
   return (
     <StyledHeader>
-      <nav role="navigation">
-        <ul>
-          <li>Hello.</li>
-        </ul>
-      </nav>
+      <Scrollspy
+        items={['section1', 'section2', 'section3', 'section4', 'section5']}
+        currentClassName="is-current"
+      >
+        <nav role="navigation">
+          <ul>
+            <li><a href="#section1">section1</a></li>
+            <li><a href="#section2">section2</a></li>
+            <li><a href="#section3">section3</a></li>
+            <li><a href="#section4">section4</a></li>
+            <li><a href="#section5">section5</a></li>
+          </ul>
+        </nav>
+      </Scrollspy>
     </StyledHeader>
   )
 }
