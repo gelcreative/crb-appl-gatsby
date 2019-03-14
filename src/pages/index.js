4e6bfaab4f9c01712ec9039aa6930d9756947039
Gel Studio <devondaviau@yahoo.ca>
2019-03-14 16:15:04 -0400

    margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const StyledIndexPage = styled.article`
     text-align: center;
     padding-top: 0;
 
-    .curbappeal-masthead-content-wrapper {
+    .curbappeal-masthead-content-wrapper:nth-child(-n+2) {
       margin-bottom: 5rem;
 
       &:first-child {
@@ -45,22 +45,22 @@ const IndexPage = () => (
           </div>
         </div>
       </section>
-      <section id="section2" style={{ height: '500px', backgroundColor: '#D3D3D3' }}>
+      <section id="section2" style={{ backgroundColor: '#D3D3D3' }}>
         <div className="container">
           <h2>Section 2</h2>
         </div>
       </section>
-      <section id="section3" style={{ height: '500px', backgroundColor: '#C0C0C0' }}>
+      <section id="section3" style={{ backgroundColor: '#C0C0C0' }}>
         <div className="container">
           <h2>Section 3</h2>
         </div>
       </section>
-      <section id="section4" style={{ height: '500px', backgroundColor: '#A9A9A9' }}>
+      <section id="section4" style={{ backgroundColor: '#A9A9A9' }}>
         <div className="container">
           <h2>Section 4</h2>
         </div>
       </section>
-      <section id="section5" style={{ height: '500px', backgroundColor: '#808080' }}>
+      <section id="section5" style={{ backgroundColor: '#808080' }}>
         <div className="container">
           <h2>Section 5</h2>
         </div>
