dc3493f8e87897d9577fd712cf4251e1d7a5fb2b
Gel Studio <devondaviau@yahoo.ca>
2019-03-14 14:57:42 -0400

    make sections full-width and put containers inside each

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,32 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <article className="container">
-      <h1>Hi people</h1>
+    <article>
       <section id="section1" style={{ height: '500px', backgroundColor: '#DCDCDC' }}>
-        <h2>Section 1</h2>
+        <div className="container">
+          <h1>Hi people</h1>
+          <h2>Section 1</h2>
+        </div>
       </section>
       <section id="section2" style={{ height: '500px', backgroundColor: '#D3D3D3' }}>
-        <h2>Section 2</h2>
+        <div className="container">
+          <h2>Section 2</h2>
+        </div>
       </section>
       <section id="section3" style={{ height: '500px', backgroundColor: '#C0C0C0' }}>
-        <h2>Section 3</h2>
+        <div className="container">
+          <h2>Section 3</h2>
+        </div>
       </section>
       <section id="section4" style={{ height: '500px', backgroundColor: '#A9A9A9' }}>
-        <h2>Section 4</h2>
+        <div className="container">
+          <h2>Section 4</h2>
+        </div>
       </section>
       <section id="section5" style={{ height: '500px', backgroundColor: '#808080' }}>
-        <h2>Section 5</h2>
+        <div className="container">
+          <h2>Section 5</h2>
+        </div>
       </section>
     </article>
   </Layout>
