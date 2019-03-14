f05b7dc194707861e86b6907770d8ec516f33aed
Gel Studio <devondaviau@yahoo.ca>
2019-03-14 10:58:43 -0400

    import hamburgers

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 import theme from './theme'
 import cssreset from './cssreset.css'
+import hamburgers from './hamburgers-master/dist/hamburgers.css'
 
 const GlobalStyle = createGlobalStyle`
   @import url(${cssreset});
+  @import url(${hamburgers});
 
   body {
     background-color: ${theme.white};
